41ccc05dffead2add6854f8e10da04a0d1ffef5f
Michael Olberg <michael.olberg@chalmers.se>
2019-02-26 08:15:29 +0100

    use command line arguments

--- main.cpp
+++ main.cpp
@@ -3,36 +3,43 @@
 
 int main(int argc, char *argv[])
 {
-    static char filename[] = "/home/olberg/R/O-097.F-9401A-2016/O-097.F-9401A-2016/O-097.F-9401A-2016-2016-06-08.apex";
-    ClassReader *reader = openClassFile(filename);
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s <filename>\n", argv[0]);
+        exit(1);
+    }
+
+    ClassReader *reader = openClassFile(argv[1]);
     std::vector<SpectrumHeader> shv;
 
-    int nscans = reader->getDirectory();
-    printf("number of spectra = %d\n", nscans);
+    if (reader) {
+        int nscans = reader->getDirectory();
+        printf("number of spectra = %d\n", nscans);
 
-    shv.reserve(nscans);
-    for (int iscan = 0; iscan < nscans; iscan++) {
-        SpectrumHeader S = reader->getHead(iscan+1);
-        shv.push_back(S);
-    }
+        shv.reserve(nscans);
+        for (int iscan = 0; iscan < nscans; iscan++) {
+            SpectrumHeader S = reader->getHead(iscan+1);
+            shv.push_back(S);
+        }
 
-    std::vector<SpectrumHeader>::const_iterator i;
-    for(i = shv.begin(); i != shv.end(); ++i) {
-        SpectrumHeader head = *i;
-        head.print();
-    }
+        std::vector<SpectrumHeader>::const_iterator i;
+        for(i = shv.begin(); i != shv.end(); ++i) {
+            SpectrumHeader head = *i;
+            head.print();
+        }
 
-    int n = 120;
-    SpectrumHeader S = reader->getHead(n);
-    S.print();
-    std::vector<double> f = reader->getFreq(n);
-    std::vector<double> d = reader->getData(n);
+        int n = 1;
+        if (argc > 2) n = atoi(argv[2]);
+        SpectrumHeader S = reader->getHead(n);
+        S.print();
+        std::vector<double> f = reader->getFreq(n);
+        std::vector<double> d = reader->getData(n);
 
-    int nchan = f.size();
-    printf("number of channels = %d\n", nchan);
-    for (int k = 0; k < nchan; k++) {
-        printf("%10.5f %10.5f\n", f.at(k), d.at(k));
-    }
-    delete reader;
+        int nchan = f.size();
+        printf("number of channels = %d\n", nchan);
+        for (int k = 0; k < nchan; k++) {
+            printf("%10.5f %10.5f\n", f.at(k), d.at(k));
+        }
+        delete reader;
+    } else exit(1);
     exit(0);
 }
